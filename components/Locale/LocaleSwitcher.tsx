2ae064074cad83cfe9eedc6e634426e0d42c8a39
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-03-31 14:32:30 +0200

    feat(web): :sparkles: add locale on home

---
+++ components/Locale/LocaleSwitcher.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+export default function LocaleSwitcher() {
+  const router = useRouter()
+  const { locales, locale: activeLocale } = router
+  const otherLocales = locales?.filter((locale) => locale !== activeLocale)
+
+  return (
+    <div>
+      <p>
+        {otherLocales?.map((locale) => {
+          const { pathname, query, asPath } = router
+          return (
+            <span key={locale}>
+              <Link href={{ pathname, query }} as={asPath} locale={locale}>
+                <a>{locale}</a>
+              </Link>
+            </span>
+          )
+        })}
+      </p>
+    </div>
+  )
+}
