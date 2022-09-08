b59d66fb7bce0cb67b61fb30f94fa51f10daaa00
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2022-09-08 11:22:19 +0200

    Add favicon

--- components/Layout/Meta.js
+++ components/Layout/Meta.js
@@ -5,26 +5,26 @@ export default function Meta() {
   return (
     <Head>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
-      {/* <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${basePath}/favicon/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${basePath}/favicon/favicon-32x32.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${basePath}/favicon/favicon-16x16.png`}
-        />
-        <link rel="manifest" href={`${basePath}/favicon/site.webmanifest`} />
-        <link rel="mask-icon" href={`${basePath}/favicon/safari-pinned-tab.svg`} color="#61223d" />
-        <link rel="shortcut icon" href={`${basePath}/favicon/favicon.ico`} />
+      <link rel="shortcut icon" href={`/favicon.ico`} />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`/favicon/apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={`/favicon/favicon-32x32.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`/favicon/favicon-16x16.png`}
+      />
+      <link rel="manifest" href={`/favicon/site.webmanifest`} />
+      {/* <link rel="mask-icon" href={`${basePath}/favicon/safari-pinned-tab.svg`} color="#61223d" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="msapplication-config" content={`${basePath}/favicon/browserconfig.xml`} /> */}
       <meta name="theme-color" content="#ffffff" />
@@ -36,10 +36,6 @@ export default function Meta() {
           }
         /> */}
       {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
-      {/* <meta
-          name="google-site-verification"
-          content="hy7CN2_apzktnGzz6OakK7D-JByUVjhT8l2CenEGWKI"
-        /> */}
     </Head>
   )
 }
